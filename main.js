0aaf65b1824f64be537c4cf871c60d6f7431957f
Sarah Morris <sarah.morris9@gmail.com>
2017-12-04 09:25:50 -0600

    Completed first exercise

---
+++ main.js
@@ -0,0 +1,16 @@
+ // Monday, December 4th 2017 - 9:15am
+
+ let employees = [
+  {name: 'Bob', department: 'sales', title: 'sales manager'},
+  {name: 'Tina', department: 'finance', title: 'director of finance'},
+  {name: 'Randy', department: 'IT', title: 'hardware guy'},
+  {name: 'Glenda', department: 'C-suite', title: 'CEO'}
+]
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(`${employees[i].name}'s job title is ${employees[i].title}.`);
+}
+
+////////////////////////////////
+
+
