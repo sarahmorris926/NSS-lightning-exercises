99a53eaaad745f8158960732c1b28181e9175877
Sarah Morris <sarah.morris9@gmail.com>
2017-12-06 09:51:23 -0600

    Added new Dec 6 exercise

--- main.js
+++ main.js
@@ -13,3 +13,51 @@
 
 ////////////////////////////////
 
+// Tuesday, December 5th 2017 - 9:15am
+
+/* Instructions: 
+    Lightning exercise for Tues, 12/5:
+    Make an object with at least three properties. One property should be a method
+    Create a new object that links to the first object.
+    Give the new object two properties of its own
+    In the console, call .hasOwnProperty() on the new object, passing in an inherited property as an argument. Does it return true or false?
+    Do it again, but this time pass in one of the properties you added to the new object. True or false?
+*/
+
+// let lightningObj = {
+//   firstName: "Sarah",
+//   lastName: "Morris",
+//   age: 25,
+//   fullName: function() {
+//     age.toString;
+//   }
+// }
+
+// let newObj = {
+//   get lightningObj() {
+//     return this.lastName;
+//   },
+//   color = "Red",
+//   number = 9
+// }
+
+// console.log(lightningObj.hasOwnProperty(firstName));
+
+
+
+///////////////////////////////
+
+// Wednesday, December 6th 2017 - 
+
+/* Instructions: 
+  Loop over every item in this array [45, “I”, true, null, “am”, 3.56, “a”, undefined, { catchphrase: “Oh hai, Mark”}, “JS”, “rockstar”]  and return a new array that contains only strings. and log out the result.
+*/
+
+
+
+let strArr = [45, 'I', true, null, 'am', 3.56, 'a', undefined, {catchphrase: 'Oh hai, Mark'}, 'JS', 'rockstar'].filter( (item) => {
+  return typeof(item) === "string";
+});
+
+console.log('string array', strArr);
+
